c438bc8bd191126086530f71d6f11cf32c774200
geimerscg <51518457+geimerscg@users.noreply.github.com>
2019-09-17 11:10:20 -0700

    Initial Upload

---
+++ frontend-nanodegree-feedreader-master/frontend-nanodegree-feedreader-master/README.md
@@ -0,0 +1,9 @@
+# Feed Reader project
+    This is a project designed to introduce me to using Jasmine as a JavaScript testing tool.
+    It runs several checks against the provided app to test the functionality of the app. To run
+    the app simply locate index.html in the zipped file and run in your browser.
+## Dependencies
+    All files required to run the app are included in the zipped project folder.
+
+## About the code
+  The code has comments before each snippet to identify the purpose and explain functionality.
---
+++ frontend-nanodegree-feedreader-master/frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
@@ -0,0 +1,75 @@
+$(function() {
+    //This is the test suite of the RSS Feeds.  It checks to verify that each feed is defined, has its url defined, as well as that each feed has a name.
+    describe('RSS Feeds', function() {
+        //checks all feeds to ensure they are defined.
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+        //checks all feeds to ensure the URL is defined.
+        it('has URL defined', function() {
+            for (let feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            }
+        });
+        //checks all feeds to ensure there is a name.
+        it('has a name', function() {
+            for (let feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+        });
+
+
+    });
+
+    //This test suite is to check the functionality of the menu. It checks to make sure it functions properly and responds to click events as intended.
+    describe('The Menu', function() {
+        //check to ensure the menu is hidden by default.
+        it('is hidden by default', function() {
+            expect(document.body.classList.contains('menu-hidden')).toBe(true);
+        });
+        //check to ensure the menu changes visibility when click events are triggered.
+        it('becomes visable when clicked', function() {
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
+
+    });
+    //This test suite is to check that the loadFeed() function has at least one .entry element in the .feed container upon finishing.
+    describe('Initial Entries', function() {
+        //ensures loadFeed() has run before the check is started.
+        beforeEach(function(done) {
+            loadFeed(1, done);
+        });
+        //check to ensure at least one .entry in the .feed container.
+        it('has at least one entry', function() {
+            let container = document.querySelectorAll('.feed .entry');
+            expect(container.length).toBeGreaterThan(0);
+        });
+
+    });
+
+    //This last test suite is to ensure that when a new feed is loaded, the content actually changes as well.
+    describe('New Feed Selection', function() {
+        var text;
+        //ensures loadFeed has run before starting the check.
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                text = $('.feed').text();
+                loadFeed(1, function() {
+                    done();
+                });
+            });
+        });
+        //check to verify the new feed has actually loaded.
+        it('actually loads a new feed', function() {
+            expect($('.feed').text()).not.toBe(text);
+        });
+    });
+
+}());
